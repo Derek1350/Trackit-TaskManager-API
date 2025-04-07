eaf282fdf11dbcdb7369aa720da2f73108e27ee3
Chittanshu Singh <chittanshusingh456@gmail.com>
2025-04-07 13:05:44 +0530

    deploy error 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from app.extensions import db, jwt, migrate, redis_client
 from app.config import get_config
+import os
+from alembic.config import Config
+from alembic import command
+
+def run_migrations():
+    try:
+        alembic_cfg = Config(os.path.join(os.path.dirname(__file__), '..', 'migrations', 'alembic.ini'))
+        command.upgrade(alembic_cfg, 'head')
+    except Exception as e:
+        print("Migration failed or already applied:", str(e))
 
 def create_app():
     app = Flask(__name__)
@@ -10,6 +20,7 @@ def create_app():
     jwt.init_app(app)
     app.redis = redis_client
     migrate.init_app(app, db)
+
     from .models import users, task_logger, task_manager
 
     from app.api.routes.routes import main_bp
@@ -20,5 +31,9 @@ def create_app():
 
     from app.api.routes.task_routes import task_bp
     app.register_blueprint(task_bp)
+
+    # ✅ Run migrations inside app context
+    with app.app_context():
+        run_migrations()
 
     return app

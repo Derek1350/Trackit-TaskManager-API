85afe6faff79335a42ffbe16267df8757a38527b
Chittanshu Singh <chittanshusingh456@gmail.com>
2025-04-07 13:54:24 +0530

    LOCAL SERVERS TO RENDER

--- app/extensions.py
+++ app/extensions.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from redis import Redis
+import os
 
 # Initialize SQLAlchemy, JWT, Migrate
 db = SQLAlchemy()
@@ -9,4 +10,7 @@ jwt = JWTManager()
 migrate = Migrate()
 
 # Initialize Redis client
-redis_client = Redis.from_url("redis://redis:6379/0")  # You can replace this with your Redis URL in the environment
+# redis_client = Redis.from_url("redis://redis:6379/0")  # You can replace this with your Redis URL in the environment
+
+redis_url = os.getenv("REDIS_URL", "redis://redis:6379/0")
+redis_client = Redis.from_url(redis_url)

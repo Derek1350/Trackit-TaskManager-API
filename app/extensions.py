4f25ee223a5cc5380c74aea67df2887f754db479
Chittanshu Singh <chittanshusingh456@gmail.com>
2025-04-07 14:25:21 +0530

    server error

--- app/extensions.py
+++ app/extensions.py
@@ -2,7 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from redis import Redis
-import os
 
 # Initialize SQLAlchemy, JWT, Migrate
 db = SQLAlchemy()
@@ -10,7 +9,4 @@ jwt = JWTManager()
 migrate = Migrate()
 
 # Initialize Redis client
-# redis_client = Redis.from_url("redis://redis:6379/0")  # You can replace this with your Redis URL in the environment
-
-redis_url = os.getenv("REDIS_URL", "redis://redis:6379/0")
-redis_client = Redis.from_url(redis_url)
+redis_client = Redis.from_url("redis://redis:6379/0")  # You can replace this with your Redis URL in the environment

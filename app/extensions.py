d4c1b5bfd642b48f4de4c6f24f6b634acf3a0f11
Chittanshu Singh <chittanshusingh456@gmail.com>
2025-04-08 15:42:24 +0530

    Changing config for deployment on render db and upstash

--- app/extensions.py
+++ app/extensions.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from redis import Redis
+import os
 
 # Initialize SQLAlchemy, JWT, Migrate
 db = SQLAlchemy()
@@ -9,4 +10,5 @@ jwt = JWTManager()
 migrate = Migrate()
 
 # Initialize Redis client
-redis_client = Redis.from_url("redis://redis:6379/0")  # You can replace this with your Redis URL in the environment
+redis_url = os.getenv("REDIS_URL")
+redis_client = Redis.from_url(redis_url)  # You can replace this with your Redis URL in the environment
